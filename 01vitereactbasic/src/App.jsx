b5290087acc487e64b6b7db7eceb33e689f98014
Mohsin Raza <mr.mohsinraza32@gmail.com>
2023-10-18 17:52:31 +0500

    react liberary

--- 01vitereactbasic/src/App.jsx
+++ 01vitereactbasic/src/App.jsx
@@ -5,11 +5,11 @@ import Chai from "./chai"
 
 function App() {
   
-
+  const userName = "Mohsin Raza"
   return (
     <>
     <Chai/>
-    <h2>I am a javascript developer</h2>
+    <h2>I am a javascript developer and my name is {userName} </h2>
     <p>My name is mohsin raza</p>
     </>
   )
--- 01vitereactbasic/src/main.jsx
+++ 01vitereactbasic/src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | Chai</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+const anotherElement = (
+    <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const anotherUser = "javed hussain"
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target:'_blank'},
+    'Click me to visit google',
+    anotherUser
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    reactElement
+    
  
 )
